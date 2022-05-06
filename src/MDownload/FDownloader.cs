f533d42a3e4744391423ba16044f8c20662c1205
Federico <federvillaf@hotmail.com>
2022-05-06 18:11:20 -0300

    Agregado MFD

---
+++ src/MDownload/FDownloader.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FiscalProto;
+using FiscalProto.Descarga;
+
+namespace MDownload
+{
+    class FDownloader
+    {
+        int cmd = 0;
+        bool decrypt = false;
+        string carpeta = null;
+        TargetBag bag;
+
+        CMD_DNL_RangoFechas startF = new CMD_DNL_RangoFechas();
+        CMD_DNL_RangoZetas startZ = new CMD_DNL_RangoZetas();
+        CMD_DNL_Enumera gloop = new CMD_DNL_Enumera();
+        CMD_DNL_Final end = new CMD_DNL_Final();
+        CMD_DNL_Cancela canc = new CMD_DNL_Cancela();
+
+        public FDownloader(int cmd, int conf, bool valid, bool borrar, string carpeta, bool decrypt)
+        {
+            _ = valid;
+            _ = borrar;
+            
+            this.cmd = cmd;
+            this.carpeta = carpeta;
+            this.decrypt = decrypt;
+
+            bag = TargetBag.FromCode(BagCode.Genesis);
+        }
+
+        private void DecryptPEMFile(string fn)
+        {
+            var result = new List<byte>();
+            var lines = File.ReadAllLines(fn);
+            for (int i = 1; i < lines.Length - 1; i++)
+            {
+                var lsrc = Convert.FromBase64String(lines[i]);
+                result.AddRange(lsrc);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var b in result)
+            {
+                if (b >= 0x20 && b < 0xff)
+                    sb.Append(((char)b).ToString());
+            }
+
+            string s = sb.ToString();
+            s = s.Substring(s.IndexOf("<?xml "));
+            int next = s.IndexOf('<', 1);
+            string k = "";
+            while (next++ < s.Length)
+            {
+                char ch = s[next];
+                if (char.IsWhiteSpace(ch))
+                    break;
+                k += ch.ToString();
+            }
+            k = "</" + k + ">";
+            next = s.IndexOf(k);
+            s = s.Substring(0, next + k.Length);
+
+            fn = Path.ChangeExtension(fn, "xml");
+            File.WriteAllText(fn, s);
+        }
+
+        private string ShortPath(string fn)
+        {
+            if (fn.Length > 70)
+                fn = fn.Substring(0, 10) + "..." + fn.Substring(fn.Length - 10);
+            return fn;
+        }
+
+        public void DoStatus()
+        {
+            using (var proto = new ProtoHandler(bag))
+            {
+                var cmd = new CMD_DNL_Info();
+                var mo = proto.ExchangePacket(cmd);
+                if (mo == null) return;
+
+                Console.WriteLine("<donwload>");
+                Console.WriteLine($"  <ctd>{cmd.Output.DescargaCTDDesde}</ctd>");
+                Console.WriteLine($"  <resu>{cmd.Output.DescargaRESUDesde}</resu>");
+                Console.WriteLine($"  <dupliA>{cmd.Output.DescargaDUPLIDesde}</dupliA>");
+                Console.WriteLine($"  <listo>{cmd.Output.ListasHasta}</listo>");
+                Console.WriteLine($"  <borrado>{cmd.Output.BorradasHasta}</borrado>");
+                Console.WriteLine("</donwload>");
+            }
+        }
+
+        private void CommonDoWork(ProtoHandler proto, CMD_Generic start, Func<CMD_Generic, string> getFileName)
+        {
+            StringBuilder data = new StringBuilder();
+            try
+            {
+                // Lanza la descarga.
+                var mo = proto.ExchangePacket(start);
+                if (mo == null || mo.ErrorCodeInt != 0)
+                {
+                    string msg =
+                        (start as CMD_DNL_RangoFechas)?.Output.ErrorCode ??
+                        (start as CMD_DNL_RangoZetas)?.Output.ErrorCode ??
+                        "<Error Desconocido>";
+
+                    Console.WriteLine($"ERROR: '{msg}'");
+                    return;
+                }
+
+                // Lazo de descarga
+                int prog = 0;
+                Console.Write("Descargando: ");
+                while (true)
+                {
+                    mo = proto.ExchangePacket(gloop);
+                    if (mo.ErrorCodeInt != 0)
+                        throw new Exception(mo.Error);
+                    data.Append(gloop.Output.Data);
+                    if (!gloop.Output.Continua)
+                        break;
+
+                    if (prog++ == 5)
+                    {
+                        Console.Write("\b\b\b\b\b     \b\b\b\b\b");
+                        prog = 0;
+                    }
+                    else
+                        Console.Write(".");
+                }
+
+                // Finaliza
+                proto.ExchangePacket(end);
+                var fn = Path.Combine(carpeta, getFileName(start));
+                Console.Write($"\r                         \r==> '{ShortPath(fn)}'\n");
+                File.WriteAllText(fn, data.ToString());
+                if (decrypt)
+                {
+                    var xml = Path.ChangeExtension(fn, "xml");
+                    Console.WriteLine($" Decrypt  '{ShortPath(fn)}' => '{ShortPath(xml)}");
+                    DecryptPEMFile(fn);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR - '{ex.Message}'");
+                try
+                {
+                    proto.ExchangePacket(canc);
+                }
+                catch { }
+            }
+        }
+    
+        public void DoWorkByFecha(int fechaDesde, int fechaHasta)
+        {
+            using (var proto = new ProtoHandler(bag))
+            {
+                startF.Input.Selector = (cmd == 1) ?
+                    DNLRF_Selector.CTD :
+                    (cmd == 2) ? DNLRF_Selector.DUPLI_A : DNLRF_Selector.RESUMEN;
+
+                startF.Input.FechaDesde = fechaDesde;
+                startF.Input.FechaHasta = (fechaHasta == 0)? fechaDesde: fechaHasta;
+                startF.Input.Zipeado = true;
+                CommonDoWork(proto, startF, c => ((CMD_DNL_RangoFechas)c).Output.NomArchivo);
+            }
+        }
+
+        public void DoWorkByZeta(int zetaDesde, int zetaHasta)
+        {
+            using (var proto = new ProtoHandler(bag))
+            {
+                startZ.Input.Selector = (cmd == 1) ?
+                    DNLRZ_Selector.CTD :
+                    (cmd == 2) ? DNLRZ_Selector.DUPLI_A : DNLRZ_Selector.RESUMEN;
+
+                startZ.Input.ZetaDesde = zetaDesde;
+                startZ.Input.ZetaHasta = (zetaHasta == 0)? zetaDesde: zetaHasta;
+                startZ.Input.Zipeado = true;
+                CommonDoWork(proto, startZ, c => ((CMD_DNL_RangoZetas)c).Output.NomArchivo);
+            }
+        }
+    }
+}
---
+++ src/MDownload/Program.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.IO;
+using FiscalProto;
+
+namespace MDownload
+{
+    class Program
+    {
+        static void ShowLogo()
+        {
+            Console.WriteLine("MFD" + MVersion.Name + " - Moretti File Downloader");
+            Console.WriteLine("Copyright (c) 2018 - Andres Moretti e Hijos S.A.\n");
+        }
+
+        static void ShowInstruc()
+        {
+            Console.WriteLine("Uso:");
+            Console.WriteLine(" mfd ctd|dupli|resu|info [-d=carpeta] [-c=conf] [-y] -zdesde=num [-zhasta=num]");
+            Console.WriteLine("  --- ó ---");
+            Console.WriteLine(" mfd ctd|dupli|resu|info [-d=carpeta] [-c=conf] [-y] -fdesde=ddmmaa [-fhasta=ddmmaa]");
+            Console.WriteLine("");
+            Console.WriteLine("\n\nParámetros:\n");
+            Console.WriteLine("ctd|dupli|resu: Tipo de archivo a descargar:");
+            Console.WriteLine("    ctd:     Cinta testigo digital.");
+            Console.WriteLine("    dupli:   Duplicado de comprobantes tipo 'A' emitidos");
+            Console.WriteLine("    resu:    Resúmen de ventas");
+            Console.WriteLine("    info:    Muestra detalles del estado de descarga de los archivos");
+            Console.WriteLine("-d=carpeta:  Opcional. Nombre de la carpeta donde se colocará la descarga.");
+            Console.WriteLine("-c=conf:     Opcional. Configuración a usar. Solo se permite 'moretti' o 'epson'");
+            Console.WriteLine("-y:          Intenta desencriptar el archivo PEM");
+            Console.WriteLine("-zdesde:     Primer número de ZETA a descargar");
+            Console.WriteLine("-zhasta:     Ultimo número de ZETA a descargar. Si no lo indica, usa -zdesde");
+            Console.WriteLine("-fdesde=fecha: Fecha de la primer jornada a descargar. Usar DDMMAA");
+            Console.WriteLine("-fhasta=fecha: Fecha de la última jornada a descargar. Si no se indica, usa -fdesde");
+            Console.WriteLine("-force:      Omite validaciones de fechas. Util al hacer una baja fiscal.");
+            Console.WriteLine("");
+            Console.WriteLine("También puede invocar a 'mfd' sin parámetros, para que se abra la ventana de descarga.");
+            Console.WriteLine("-fdesde/-fhasta son mutuamente exclusivas con -zdesde/-zhasta");
+
+        }
+
+        static bool ValidFecha(int fecha)
+        {
+            return (fecha > 0);
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            int cmd = 0;
+            int conf = 0;
+            bool valid = true;
+            bool borrar = false;
+            string carpeta = null;
+            bool decrypt = false;
+            int fechaDesde = 0, fechaHasta = 0;
+            int zetaDesde = 0, zetaHasta = 0;
+
+            ShowLogo();
+            if (args.Length == 1 && (args[0] == "-?" || args[0] == "/?"))
+            {
+                ShowInstruc();
+                return;
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                var arg = args[i].Trim().ToLower();
+                if (arg == "ctd" || arg == "dupli" || arg == "resu" || arg == "info")
+                {
+                    if (cmd != 0)
+                    {
+                        Console.WriteLine("ERROR - Solo se permite uno de 'ctd' o 'dupli' o 'resu' o 'info'");
+                        valid = false;
+                        break;
+                    }
+                    cmd = (arg == "ctd") ? 1 : ((arg == "dupli") ? 2 : ((arg == "info")? 4: 3));
+                }
+                else if (arg == "-y")
+                {
+                    if (decrypt)
+                    {
+                        Console.WriteLine("ERROR - '-y' solo se puede indicar una vez");
+                        valid = false;
+                        break;
+                    }
+                    decrypt = true;
+                }
+                else if (arg.StartsWith("-c="))
+                {
+                    if (conf != 0)
+                    {
+                        Console.WriteLine("ERROR - La opción '-c=...' solo se permite una vez");
+                        valid = false;
+                        break;
+                    }
+
+                    if (arg == "-c=moretti")
+                        conf = 1;
+                    else if (arg == "-c=epson")
+                        conf = 2;
+                    else
+                    {
+                        Console.WriteLine("ERROR - '-c=...' invalido. Solo se acepta '-c=moretti' o '-c=epson'");
+                        valid = false;
+                        break;
+                    }
+
+                }
+                else if (arg.StartsWith("-d="))
+                {
+                    if (carpeta != null)
+                    {
+                        Console.WriteLine("ERROR - '-d=...' solo se puede indicar una sola vez");
+                        valid = false;
+                        break;
+                    }
+
+                    carpeta = arg.Substring(3);
+                    if (!Directory.Exists(carpeta))
+                    {
+                        Console.WriteLine($"ERROR - La carpeta '{carpeta}' no existe");
+                        valid = false;
+                        break;
+                    }
+                }
+                else if (arg.StartsWith("-fdesde="))
+                {
+                    if (fechaDesde > 0)
+                    {
+                        Console.WriteLine("ERROR - '-fdesde' solo se puede indicar una vez");
+                        valid = false;
+                        break;
+                    }
+                    else if(zetaDesde > 0)
+                    {
+                        Console.WriteLine("ERROR - No puede usar '-fdesde' si ya us '-zdesde");
+                        valid = false;
+                        break;
+                    }
+                    if (!int.TryParse(arg.Substring(8), out fechaDesde) || !ValidFecha(fechaDesde))
+                    {
+                        Console.WriteLine("ERROR - El valor para '-fdesde=' no es valido. Use DDMMAA");
+                        valid = false;
+                        break;
+                    }
+                }
+                else if (arg.StartsWith("-fhasta="))
+                {
+                    if (fechaHasta > 0)
+                    {
+                        Console.WriteLine("ERROR - '-fhasta' solo se puede indicar una vez");
+                        valid = false;
+                        break;
+                    }
+                    else if(fechaDesde <= 0)
+                    {
+                        Console.WriteLine("ERROR - No puede usar '-fhasta' si todavía no use '-fdesde'");
+                        valid = false;
+                        break;
+                    }
+                    else if (zetaDesde > 0)
+                    {
+                        Console.WriteLine("ERROR - No puede usar '-fhasta' si ya uso '-zdesde");
+                        valid = false;
+                        break;
+                    }
+                    if (!int.TryParse(arg.Substring(8), out fechaHasta) || !ValidFecha(fechaHasta))
+                    {
+                        Console.WriteLine("ERROR - El valor para '-fhasta=' no es valido. Use DDMMAA");
+                        valid = false;
+                        break;
+                    }
+                }
+                else if (arg.StartsWith("-zdesde="))
+                {
+                    if (zetaDesde > 0)
+                    {
+                        Console.WriteLine("ERROR - '-zdesde' solo se puede indicar una vez");
+                        valid = false;
+                        break;
+                    }
+                    else if (fechaDesde > 0)
+                    {
+                        Console.WriteLine("ERROR - No puede usar '-zdesde' si ya usó '-fdesde");
+                        valid = false;
+                        break;
+                    }
+                    if (!int.TryParse(arg.Substring(8), out zetaDesde))
+                    {
+                        Console.WriteLine("ERROR - El valor para '-zdesde=' no es valido.");
+                        valid = false;
+                        break;
+                    }
+                }
+                else if (arg.StartsWith("-zhasta="))
+                {
+                    if (zetaHasta > 0)
+                    {
+                        Console.WriteLine("ERROR - '-zhasta' solo se puede indicar una vez");
+                        valid = false;
+                        break;
+                    }
+                    else if (zetaDesde <= 0)
+                    {
+                        Console.WriteLine("ERROR - No puede usar '-zhasta' si todavía no use '-zdesde'");
+                        valid = false;
+                        break;
+                    }
+                    else if (fechaDesde > 0)
+                    {
+                        Console.WriteLine("ERROR - No puede usar '-zhasta' si ya uso '-fdesde");
+                        valid = false;
+                        break;
+                    }
+                    if (!int.TryParse(arg.Substring(8), out zetaHasta))
+                    {
+                        Console.WriteLine("ERROR - El valor para '-zhasta=' no es valido.");
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (cmd == 0)
+            {
+                Console.WriteLine("ERROR - Debe indicar uno de 'ctd' o 'dupli' o 'resu' o 'info'");
+                valid = false;
+            }
+
+            if (fechaDesde == 0 && zetaDesde == 0 && cmd < 4)
+            {
+                Console.WriteLine("ERROR - Debe indicar la opción '-fdesde=...' o sino '-zdesde=...'");
+                valid = false;
+            }
+
+            if (!valid)
+            {
+                Console.WriteLine();
+                ShowInstruc();
+            }
+            else
+            {
+                if (conf == 0) conf = 1;
+                if (carpeta == null) carpeta = ".\\";
+
+                var fd = new FDownloader(cmd, conf, valid, borrar, carpeta, decrypt);
+                if (cmd == 4)
+                    fd.DoStatus();
+                else if (zetaDesde > 0)
+                    fd.DoWorkByZeta(zetaDesde, zetaHasta);
+                else
+                    fd.DoWorkByFecha(fechaDesde, fechaHasta);
+            }
+
+#if DEBUG
+            Console.WriteLine("Listo. Pulsar una tecla para salir...");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
